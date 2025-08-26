eac9c52cc7f5119178b483fcccd7bc7c5e73b066
S01101S <saimpreetsingh3085@gmail.com>
2025-08-26 20:45:01 +0100

    mouse interactions added

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ public:
     sf::Vector2f position;
     sf::Vector2f velocity;
     sf::Vector2f acceleration;
-    sf::RectangleShape shape;
+    sf::ConvexShape shape;
 
     Boid(float x, float y, std::mt19937 generator)
     {
@@ -22,10 +22,28 @@ public:
         velocity = sf::Vector2f(distribution(generator), distribution(generator));
         acceleration = sf::Vector2f(0.f, 0.f);
 
-        shape.setSize(sf::Vector2f(15.f, 2.f));
-        shape.setOrigin(7.5f, 1.f);
-        shape.setFillColor(sf::Color::White);
+        const float sizeScale = generateSize(generator);
+        sf::Vector2f newSize (15.f * sizeScale, 2.f * sizeScale);
+        shape.setPointCount(3);
+        shape.setPoint(0,sf::Vector2f(10, 0));
+        shape.setPoint(1, sf::Vector2f(-5, -5));
+        shape.setPoint(2, sf::Vector2f(-5, 5));
+        shape.setOrigin(0, 0);
+        shape.setScale(sizeScale, sizeScale);
+        shape.setFillColor(sf::Color(generateColor(generator), generateColor(generator), generateColor(generator)));
         shape.setPosition(position);
+    }
+
+    int generateColor(std::mt19937& generator)
+    {
+        std::uniform_int_distribution<int> distribution(0, 255);
+        return distribution(generator);
+    }
+
+    float generateSize(std::mt19937& generator)
+    {
+        std::uniform_real_distribution<float> distribution(0.8f, 1.2f);
+        return distribution(generator);
     }
 
     void updateBoidPosition(float windowWidth, float windowHeight)
@@ -66,71 +84,89 @@ public:
         }
     }
 
-    void flockRules(std::vector<Boid>& boids)
+    void flockRules(std::vector<Boid>& boids, sf::Vector2f mousePosition)
     {
         sf::Vector2f totalPosition = sf::Vector2f(0.f, 0.f);
         sf::Vector2f totalVelocity = sf::Vector2f(0.f, 0.f);
 
-        for (auto& boid : boids)
+        const float mouseDx = mousePosition.x - this->position.x;
+        const float mouseDy = mousePosition.y - this->position.y;
+        float mouseDistance = sqrt((mouseDx * mouseDx) + (mouseDy * mouseDy));
+
+        if (mouseDistance < 100.f)
         {
-            totalPosition += boid.position;
-        }
+            sf::Vector2f fleeDirection = this->position - mousePosition;
+            float magnitudeOfFleeDirection = sqrt((fleeDirection.x * fleeDirection.x) + (fleeDirection.y * fleeDirection.y));
 
-        sf::Vector2f centerOfFlock = totalPosition / static_cast<float>(boids.size());
-        sf::Vector2f steeringDirection = centerOfFlock - position;
-        float magnitudeOfSteeringDirection = sqrt(steeringDirection.x*steeringDirection.x + steeringDirection.y*steeringDirection.y);
-        sf::Vector2f normalisedSteeringDirectionForce = steeringDirection / magnitudeOfSteeringDirection;
-
-        normalisedSteeringDirectionForce *= 0.05f;
-        acceleration += normalisedSteeringDirectionForce;
-
-        for (auto& neighbourBoid : boids)
-        {
-            if (this == &neighbourBoid)
+            if (magnitudeOfFleeDirection > 0)
             {
-                continue;
-            }
-
-            float dx = (this->position.x - neighbourBoid.position.x);
-            float dy = (this->position.y - neighbourBoid.position.y);
-            float distance = sqrt((dx * dx) + (dy * dy));
-
-            if (distance < 25.f)
-            {
-                sf::Vector2f steerAwayDirection = this->position - neighbourBoid.position;
-                float magnitudeOfSteerAwayDirection = sqrt(steerAwayDirection.x * steerAwayDirection.x + steerAwayDirection.y * steerAwayDirection.y);
-                sf::Vector2f normalisedSteerAwayDirection = steerAwayDirection / magnitudeOfSteerAwayDirection;
-                normalisedSteerAwayDirection *= 0.05f;
-                this->acceleration += normalisedSteerAwayDirection;
+                sf::Vector2f normalisedFleeingdirection = fleeDirection / magnitudeOfFleeDirection;
+                normalisedFleeingdirection *= 1.f;
+                this->acceleration += normalisedFleeingdirection;
             }
         }
-
-        for (auto& boid : boids)
+        else
         {
-            totalVelocity += boid.velocity;
-        }
+            for (auto& boid : boids)
+            {
+                totalPosition += boid.position;
+            }
 
+            sf::Vector2f centerOfFlock = totalPosition / static_cast<float>(boids.size());
+            sf::Vector2f steeringDirection = centerOfFlock - position;
+            float magnitudeOfSteeringDirection = sqrt(steeringDirection.x*steeringDirection.x + steeringDirection.y*steeringDirection.y);
+            sf::Vector2f normalisedSteeringDirectionForce = steeringDirection / magnitudeOfSteeringDirection;
 
-        sf::Vector2f averageVelocity = totalVelocity / static_cast<float>(boids.size());
-        sf::Vector2f velocitySteering = averageVelocity - this->velocity;
-        float magnitudeOfVelocitySteering = sqrt(velocitySteering.x * velocitySteering.x + velocitySteering.y * velocitySteering.y);
-        if (magnitudeOfSteeringDirection > 0)
-        {
-            sf::Vector2f normalisedVelocitySteering = velocitySteering / magnitudeOfVelocitySteering;
-            normalisedVelocitySteering *= 0.05f;
-            this->acceleration += normalisedVelocitySteering;
+            normalisedSteeringDirectionForce *= 0.05f;
+            acceleration += normalisedSteeringDirectionForce;
+
+            for (auto& neighbourBoid : boids)
+            {
+                if (this == &neighbourBoid)
+                {
+                    continue;
+                }
+
+                float dx = (this->position.x - neighbourBoid.position.x);
+                float dy = (this->position.y - neighbourBoid.position.y);
+                float distance = sqrt((dx * dx) + (dy * dy));
+
+                if (distance < 25.f)
+                {
+                    sf::Vector2f steerAwayDirection = this->position - neighbourBoid.position;
+                    float magnitudeOfSteerAwayDirection = sqrt(steerAwayDirection.x * steerAwayDirection.x + steerAwayDirection.y * steerAwayDirection.y);
+                    sf::Vector2f normalisedSteerAwayDirection = steerAwayDirection / magnitudeOfSteerAwayDirection;
+                    normalisedSteerAwayDirection *= 0.05f;
+                    this->acceleration += normalisedSteerAwayDirection;
+                }
+            }
+
+            for (auto& boid : boids)
+            {
+                totalVelocity += boid.velocity;
+            }
+
+            sf::Vector2f averageVelocity = totalVelocity / static_cast<float>(boids.size());
+            sf::Vector2f velocitySteering = averageVelocity - this->velocity;
+            float magnitudeOfVelocitySteering = sqrt(velocitySteering.x * velocitySteering.x + velocitySteering.y * velocitySteering.y);
+            if (magnitudeOfSteeringDirection > 0)
+            {
+                sf::Vector2f normalisedVelocitySteering = velocitySteering / magnitudeOfVelocitySteering;
+                normalisedVelocitySteering *= 0.05f;
+                this->acceleration += normalisedVelocitySteering;
+            }
         }
 
         const float accelerationMagnitude = sqrt((this->acceleration.x * this->acceleration.x) + (this->acceleration.y * this->acceleration.y));
 
-        if (accelerationMagnitude > 0.1f)
+        if (accelerationMagnitude > 0.5f)
         {
-            this->acceleration = (this->acceleration / accelerationMagnitude) * 0.1f;
+            this->acceleration = (this->acceleration / accelerationMagnitude) * 0.5f;
         }
 
     }
 };
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 int main()
 {
     const float windowHeight = 600.f;
@@ -168,12 +204,14 @@ int main()
             }
         }
 
+        sf::Vector2i pixelPosition = sf::Mouse::getPosition(window);
+        sf::Vector2f mousePosition = window.mapPixelToCoords(pixelPosition);
 
         window.clear(sf::Color::Black);
 
         for (auto& currentBoid : boidList)
         {
-            currentBoid.flockRules(boidList);
+            currentBoid.flockRules(boidList, mousePosition);
             currentBoid.updateBoidPosition(windowWidth, windowHeight);
             window.draw(currentBoid.shape);
         }

b423ee277602d24f24852c995a6bd4e88c312f07
S01101S <saimpreetsingh3085@gmail.com>
2025-09-02 13:25:16 +0100

    created interactive UI

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ public:
     sf::Vector2f acceleration;
     sf::ConvexShape shape;
 
-    Boid(float x, float y, std::mt19937 generator)
+    Boid(float x, float y, std::mt19937& generator)
     {
         std::uniform_real_distribution<float> distribution(-2.f, 2.f);
 
@@ -88,7 +88,7 @@ public:
         }
     }
 
-    void flockRules(std::vector<Boid>& boids, sf::Vector2f mousePosition, bool blackHoleActive, sf::Vector2f blackHolePosition)
+    void flockRules(std::vector<Boid>& boids, sf::Vector2f mousePosition, bool blackHoleActive, sf::Vector2f blackHolePosition, bool cohesionActive, bool separationActive, bool alignmentActive)
     {
         sf::Vector2f totalPosition = sf::Vector2f(0.f, 0.f);
         sf::Vector2f totalVelocity = sf::Vector2f(0.f, 0.f);
@@ -136,13 +136,19 @@ public:
                 totalPosition += boid.position;
             }
 
-            sf::Vector2f centerOfFlock = totalPosition / static_cast<float>(boids.size());
-            sf::Vector2f steeringDirection = centerOfFlock - position;
-            float magnitudeOfSteeringDirection = sqrt(steeringDirection.x*steeringDirection.x + steeringDirection.y*steeringDirection.y);
-            sf::Vector2f normalisedSteeringDirectionForce = steeringDirection / magnitudeOfSteeringDirection;
+            if (cohesionActive)
+            {
+                sf::Vector2f centerOfFlock = totalPosition / static_cast<float>(boids.size());
+                sf::Vector2f steeringDirection = centerOfFlock - position;
+                float magnitudeOfSteeringDirection = sqrt(steeringDirection.x*steeringDirection.x + steeringDirection.y*steeringDirection.y);
 
-            normalisedSteeringDirectionForce *= 0.05f;
-            acceleration += normalisedSteeringDirectionForce;
+                if (magnitudeOfSteeringDirection > 0)
+                {
+                    sf::Vector2f normalisedSteeringDirectionForce = steeringDirection / magnitudeOfSteeringDirection;
+                    normalisedSteeringDirectionForce *= 0.05f;
+                    acceleration += normalisedSteeringDirectionForce;
+                }
+            }
 
             for (auto& neighbourBoid : boids)
             {
@@ -151,18 +157,26 @@ public:
                     continue;
                 }
 
-                float dx = (this->position.x - neighbourBoid.position.x);
-                float dy = (this->position.y - neighbourBoid.position.y);
-                float distance = sqrt((dx * dx) + (dy * dy));
-
-                if (distance < 25.f)
+                if (separationActive)
                 {
-                    sf::Vector2f steerAwayDirection = this->position - neighbourBoid.position;
-                    float magnitudeOfSteerAwayDirection = sqrt(steerAwayDirection.x * steerAwayDirection.x + steerAwayDirection.y * steerAwayDirection.y);
-                    sf::Vector2f normalisedSteerAwayDirection = steerAwayDirection / magnitudeOfSteerAwayDirection;
-                    normalisedSteerAwayDirection *= 0.05f;
-                    this->acceleration += normalisedSteerAwayDirection;
+                    float dx = (this->position.x - neighbourBoid.position.x);
+                    float dy = (this->position.y - neighbourBoid.position.y);
+                    float distance = sqrt((dx * dx) + (dy * dy));
+
+                    if (distance < 25.f)
+                    {
+                        sf::Vector2f steerAwayDirection = this->position - neighbourBoid.position;
+                        float magnitudeOfSteerAwayDirection = sqrt(steerAwayDirection.x * steerAwayDirection.x + steerAwayDirection.y * steerAwayDirection.y);
+
+                        if (magnitudeOfSteerAwayDirection > 0)
+                        {
+                            sf::Vector2f normalisedSteerAwayDirection = steerAwayDirection / magnitudeOfSteerAwayDirection;
+                            normalisedSteerAwayDirection *= 0.05f;
+                            this->acceleration += normalisedSteerAwayDirection;
+                        }
+                    }
                 }
+
             }
 
             for (auto& boid : boids)
@@ -170,14 +184,17 @@ public:
                 totalVelocity += boid.velocity;
             }
 
-            sf::Vector2f averageVelocity = totalVelocity / static_cast<float>(boids.size());
-            sf::Vector2f velocitySteering = averageVelocity - this->velocity;
-            float magnitudeOfVelocitySteering = sqrt(velocitySteering.x * velocitySteering.x + velocitySteering.y * velocitySteering.y);
-            if (magnitudeOfSteeringDirection > 0)
+            if (alignmentActive)
             {
-                sf::Vector2f normalisedVelocitySteering = velocitySteering / magnitudeOfVelocitySteering;
-                normalisedVelocitySteering *= 0.05f;
-                this->acceleration += normalisedVelocitySteering;
+                sf::Vector2f averageVelocity = totalVelocity / static_cast<float>(boids.size());
+                sf::Vector2f velocitySteering = averageVelocity - this->velocity;
+                float magnitudeOfVelocitySteering = sqrt(velocitySteering.x * velocitySteering.x + velocitySteering.y * velocitySteering.y);
+                if (magnitudeOfVelocitySteering > 0)
+                {
+                    sf::Vector2f normalisedVelocitySteering = velocitySteering / magnitudeOfVelocitySteering;
+                    normalisedVelocitySteering *= 0.05f;
+                    this->acceleration += normalisedVelocitySteering;
+                }
             }
         }
 
@@ -191,10 +208,194 @@ public:
     }
 };
 
+
+
+
+class UserInterface
+{
+
+public:
+    sf::Font myFont;
+    sf::Text boidCountText;
+    sf::Text cohesionActiveText;
+    sf::Text separationActiveText;
+    sf::Text alignmentActiveText;
+    sf::Text addBoidCountText;
+    sf::Text minusBoidCountText;
+    sf::RectangleShape cohesionButton;
+    sf::RectangleShape separationButton;
+    sf::RectangleShape alignmentButton;
+    sf::RectangleShape minusBoidCountButton;
+    sf::RectangleShape addBoidCountButton;
+
+
+    UserInterface()
+    {
+        if (!myFont.loadFromFile("arial.ttf"))
+        {
+            std::cout << "Error loading arial.ttf" << std::endl;
+            exit(1);
+        }
+
+        setUpText(boidCountText);
+        setUpText(cohesionActiveText);
+        setUpText(separationActiveText);
+        setUpText(alignmentActiveText);
+        setUpTextForAddAndMinus(addBoidCountText);
+        setUpTextForAddAndMinus(minusBoidCountText);
+
+
+        separationButton.setSize(sf::Vector2f(15, 15));
+        separationButton.setPosition(150, 30);
+
+        cohesionButton.setSize(sf::Vector2f(15, 15));
+        cohesionButton.setPosition(150, 50);
+
+        alignmentButton.setSize(sf::Vector2f(15, 15));
+        alignmentButton.setPosition(150, 70);
+
+        addBoidCountButton.setSize(sf::Vector2f(10, 10));
+        addBoidCountButton.setPosition(120, 13);
+
+        minusBoidCountButton.setSize(sf::Vector2f(10, 10));
+        minusBoidCountButton.setPosition(100, 13);
+    }
+
+    void update(int boidCount, bool cohesionActive, bool separationActive, bool alignmentActive)
+    {
+        boidCountText.setString("Boid Count: " + std::to_string(boidCount));
+        boidCountText.setPosition(10, 10);
+
+        addBoidCountText.setString("+");
+        addBoidCountText.setPosition(120, 9);
+
+        minusBoidCountText.setString("-");
+        minusBoidCountText.setPosition(100, 9);
+
+        separationActiveText.setString(std::string("Separation Active: ") + (separationActive ? "ON" : "OFF"));
+        separationActiveText.setPosition(10, 30);
+
+        cohesionActiveText.setString(std::string("Cohesion Active: ") + (cohesionActive ? "ON" : "OFF"));
+        cohesionActiveText.setPosition(10, 50);
+
+        alignmentActiveText.setString(std::string("Alignment Active: ") + (alignmentActive ? "ON" : "OFF"));
+        alignmentActiveText.setPosition(10, 70);
+
+        if (cohesionActive == true)
+        {
+            cohesionButton.setFillColor(sf::Color::Green);
+        }
+        else if (cohesionActive == false)
+        {
+            cohesionButton.setFillColor(sf::Color::Red);
+        }
+
+        if (separationActive == true)
+        {
+            separationButton.setFillColor(sf::Color::Green);
+        }
+        else if (separationActive == false)
+        {
+            separationButton.setFillColor(sf::Color::Red);
+        }
+
+        if (alignmentActive == true)
+        {
+            alignmentButton.setFillColor(sf::Color::Green);
+        }
+
+        else if (alignmentActive == false)
+        {
+            alignmentButton.setFillColor(sf::Color::Red);
+        }
+    }
+
+    int handleClick(sf::Event& event, bool& cohesionActive, bool& separationActive, bool& alignmentActive)
+    {
+
+        if (event.type == sf::Event::MouseButtonPressed)
+        {
+            if (cohesionButton.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+            {
+                cohesionActive = !cohesionActive;
+                return 2;
+            }
+
+            if (separationButton.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+            {
+                separationActive = !separationActive;
+                return 2;
+            }
+
+            if (alignmentButton.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+            {
+                alignmentActive = !alignmentActive;
+                return 2;
+            }
+
+            if (addBoidCountButton.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+            {
+                return 1;
+            }
+
+            if (minusBoidCountButton.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+            {
+                return -1;
+            }
+        }
+
+        return 0;
+    }
+
+    void draw(sf::RenderWindow& window)
+    {
+        window.draw(boidCountText);
+        window.draw(separationActiveText);
+        window.draw(cohesionActiveText);
+        window.draw(alignmentActiveText);
+
+        window.draw(cohesionButton);
+        window.draw(separationButton);
+        window.draw(alignmentButton);
+        window.draw(addBoidCountButton);
+        window.draw(minusBoidCountButton);
+
+        window.draw(addBoidCountText);
+        window.draw(minusBoidCountText);
+    }
+
+
+private:
+    void setUpText(sf::Text& text)
+    {
+        text.setFont(myFont);
+        text.setCharacterSize(13);
+        text.setFillColor(sf::Color::White);
+    }
+
+    void setUpTextForAddAndMinus(sf::Text& text)
+    {
+        text.setFont(myFont);
+        text.setCharacterSize(15);
+        text.setFillColor(sf::Color::Black);
+    }
+
+};
+
+
+
+
 int main()
 {
     const float windowHeight = 600.f;
     const float windowWidth = 800.f;
+    int boidCount = 50;
+    bool cohesionActive = true;
+    bool separationActive = true;
+    bool alignmentActive = true;
+    UserInterface boidUI;
+    sf::RenderTexture renderTexture;
+    renderTexture.create(windowWidth, windowHeight);
 
     std::random_device rd;
     std::mt19937 generator(rd());
@@ -204,11 +405,10 @@ int main()
 
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Boid Simulation");
     window.setFramerateLimit(60);
-    window.setVerticalSyncEnabled(true);
 
     std::vector<Boid> boidList;
 
-    for (int i=0; i<50; i++)
+    for (int i=0; i<boidCount; i++)
     {
         const float boidXPosition = distributionX(generator);
         const float boidYPosition = distributionY(generator);
@@ -221,7 +421,7 @@ int main()
     std::uniform_real_distribution<float> starDistributionY(0, windowHeight);
     std::uniform_real_distribution<float> starRadius(1.f, 1.5f);
 
-    for (int i=0; i<200; i++)
+    for (int i=0; i<100; i++)
     {
         sf::CircleShape newStar;
 
@@ -251,10 +451,12 @@ int main()
     accretionDisk.setOrigin(50.f, 1.f);
     float accretionDiskRotation = 0.f;
 
+    sf::RectangleShape fadeRect(sf::Vector2f(windowWidth, windowHeight));
+    fadeRect.setFillColor(sf::Color(0, 0, 0, 50));
+
     while (window.isOpen())
     {
         sf::Event event;
-
 
         while (window.pollEvent(event))
         {
@@ -263,53 +465,84 @@ int main()
                 window.close();
             }
 
-            if (event.type == sf::Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == sf::Mouse::Left)
-                {
-                    blackHoleActive = true;
-                    blackHolePosition = sf::Vector2f(event.mouseButton.x, event.mouseButton.y);
+            int uiSwitchToggle = boidUI.handleClick(event, cohesionActive, separationActive, alignmentActive);
 
+            if (uiSwitchToggle == 0)
+            {
+                if (event.type == sf::Event::MouseButtonPressed)
+                {
+                    if (event.mouseButton.button == sf::Mouse::Left)
+                    {
+                        blackHoleActive = true;
+                        sf::Vector2i mousePixelPosition(event.mouseButton.x, event.mouseButton.y);
+                        blackHolePosition = window.mapPixelToCoords(mousePixelPosition);
+
+                    }
+                }
+
+                if (event.type == sf::Event::MouseButtonReleased)
+                {
+                    blackHoleActive = false;
                 }
             }
 
-            if (event.type == sf::Event::MouseButtonReleased)
+            else if (uiSwitchToggle == 1)
             {
-                blackHoleActive = false;
+                const float addNewBoidXPosition = distributionX(generator);
+                const float addNewBoidYPosition = distributionY(generator);
+                Boid newBoid(addNewBoidXPosition, addNewBoidYPosition, generator);
+                boidList.push_back(newBoid);
+
+                boidCount = boidList.size();
+            }
+
+            else if (uiSwitchToggle == -1)
+            {
+                boidList.pop_back();
+                boidCount = boidList.size();
             }
 
         }
 
-
         sf::Vector2i pixelPosition = sf::Mouse::getPosition(window);
         sf::Vector2f mousePosition = window.mapPixelToCoords(pixelPosition);
 
-        sf::RectangleShape fadeRect(sf::Vector2f(windowWidth, windowHeight));
-        fadeRect.setFillColor(sf::Color(0, 0, 0, 70));
+        for (auto& currentBoid : boidList)
+        {
+            currentBoid.flockRules(boidList, mousePosition, blackHoleActive, blackHolePosition, cohesionActive, separationActive, alignmentActive);
+            currentBoid.updateBoidPosition(windowWidth, windowHeight);
+        }
 
-        window.draw(fadeRect);
-
+        renderTexture.draw(fadeRect);
         if (blackHoleActive)
         {
             blackHoleEffect.setPosition(blackHolePosition);
             accretionDisk.setPosition(blackHolePosition);
             accretionDiskRotation += 0.5f;
             accretionDisk.setRotation(accretionDiskRotation);
-            window.draw(blackHoleEffect);
-            window.draw(accretionDisk);
+            renderTexture.draw(blackHoleEffect);
+            renderTexture.draw(accretionDisk);
         }
 
         for (auto& star : starField)
         {
-            window.draw(star);
+            renderTexture.draw(star);
         }
 
-        for (auto& currentBoid : boidList)
+        for (auto& boid : boidList)
         {
-            currentBoid.flockRules(boidList, mousePosition, blackHoleActive, blackHolePosition);
-            currentBoid.updateBoidPosition(windowWidth, windowHeight);
-            window.draw(currentBoid.shape);
+            renderTexture.draw(boid.shape);
         }
+
+        renderTexture.display();
+
+        window.clear(sf::Color::Black);
+
+        sf::Sprite trailSprite(renderTexture.getTexture());
+        window.draw(trailSprite);
+
+        boidUI.update(boidCount, cohesionActive, separationActive, alignmentActive);
+        boidUI.draw(window);
 
         window.display();
 
